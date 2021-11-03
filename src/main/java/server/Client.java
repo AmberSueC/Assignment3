57040cda6681cccddf80c0d79ffba29b4ef25eb6
AmberSueC <84486662+AmberSueC@users.noreply.github.com>
2021-11-03 13:31:00 -0700

    Initial Client and Server Interaction

--- src/main/java/server/Client.java
+++ src/main/java/server/Client.java
@@ -1,4 +1,47 @@
 package server;
 
+import java.io.*;
+import java.net.*;
+
 public class Client {
+
+        public static void main(String args[]) {
+
+            try{
+                // Create client socket
+                Socket s = new Socket("localhost", 8080);
+
+                // to send data to the server
+                DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+
+                // to read data coming from the server
+                BufferedReader br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+                // to read data from the keyboard
+                BufferedReader kb = new BufferedReader(new InputStreamReader(System.in));
+                String str, str1;
+
+                // repeat as long as exit
+                // is not typed at client
+                while (!(str = kb.readLine()).equals("exit")) {
+
+                    // send to the server
+                    dos.writeBytes(str + "\n");
+
+                    // receive from the server
+                    str1 = br.readLine();
+
+                    System.out.println(str1);
+                }
+
+                // close connection.
+                dos.close();
+                br.close();
+                kb.close();
+                s.close();
+            } catch (Exception e){
+                System.out.println("Unknown Exception in Client\n");
+                e.printStackTrace();
+            }
+        }
 }
